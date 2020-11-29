46a7df1cad8d7fd1f4fc3a32f5fed8967cfe4169
Alex Withington-Smith <alex@awithsmith.com>
2020-11-29 09:34:17 +0000

    refactored Frame to more elegantly error handle

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,9 +1,21 @@
+# frozen_string_literal: true
+
 class Frame
-  attr_accessor :ball_1_pins, :ball_2_pins
+  attr_reader :ball_1_pins, :ball_2_pins
 
   def initialize
     @ball_1_pins = 0
     @ball_2_pins = 0
+  end
+
+  def add_pins_ball1(score)
+    error_check(score)
+    @ball_1_pins = score
+  end
+
+  def add_pins_ball2(score)
+    error_check(score)
+    @ball_2_pins = score
   end
 
   def modifier
@@ -14,5 +26,11 @@ class Frame
 
   def total_pins
     ball_1_pins + ball_2_pins
+  end
+
+  private
+
+  def error_check(score)
+    raise 'Invalid number of pins' if score + @ball_1_pins > 10
   end
 end
--- spec/unit/frame_spec.rb
+++ spec/unit/frame_spec.rb
@@ -1,46 +1,67 @@
+# frozen_string_literal: true
+
 require 'frame'
 
 describe Frame do
-  it "has ball_1_pins and ball_2_pins read writable" do
-    example_frame = Frame.new
-    example_frame.ball_1_pins = 3
-    example_frame.ball_2_pins = 3
-    expect(example_frame.ball_1_pins).to eq(3)
-    expect(example_frame.ball_2_pins).to eq(3)
+  describe '#add_pins_ball1' do
+    it 'adds a readable pin count to ball 1' do
+      example_frame = Frame.new
+      example_frame.add_pins_ball1(3)
+      expect(example_frame.ball_1_pins).to eq(3)
+    end
+
+    it 'throws an error if the pin count is above 10' do
+      example_frame = Frame.new
+      expect { example_frame.add_pins_ball1(11) }.to raise_error('Invalid number of pins')
+    end
   end
 
-  describe "#modifier" do
-    it "returns :strike when 10 pins down in ball 1" do
+  describe '#add_pins_ball2' do
+    it 'adds a readable pin count to ball 2' do
       example_frame = Frame.new
-      example_frame.ball_1_pins = 10
+      example_frame.add_pins_ball2(3)
+      expect(example_frame.ball_2_pins).to eq(3)
+    end
+
+    it 'throws an error if total would be over 10' do
+      example_frame = Frame.new
+      example_frame.add_pins_ball1(9)
+      expect { example_frame.add_pins_ball2(5) }.to raise_error('Invalid number of pins')
+    end
+  end
+
+  describe '#modifier' do
+    it 'returns :strike when 10 pins down in ball 1' do
+      example_frame = Frame.new
+      example_frame.add_pins_ball1(10)
       expect(example_frame.modifier).to eq(:strike)
     end
 
-    it "returns :spare when total pins 10 over 2 balls" do
+    it 'returns :spare when total pins 10 over 2 balls' do
       example_frame = Frame.new
-      example_frame.ball_1_pins = 7
-      example_frame.ball_2_pins = 3
+      example_frame.add_pins_ball1(7)
+      example_frame.add_pins_ball2(3)
       expect(example_frame.modifier).to eq(:spare)
     end
 
-    it "returns nil when neither condition met" do
+    it 'returns nil when neither condition met' do
       example_frame = Frame.new
-      example_frame.ball_1_pins = 0
-      example_frame.ball_2_pins = 4
+      example_frame.add_pins_ball1(3)
+      example_frame.add_pins_ball2(4)
       expect(example_frame.modifier).to eq(nil)
     end
   end
 
-  describe "#total_pins" do
-    it "returns 0 if there are no pins" do
+  describe '#total_pins' do
+    it 'returns 0 if there are no pins' do
       expect(Frame.new.total_pins).to eq(0)
     end
 
-    it "returns correctly when there are two results" do
+    it 'returns correctly when there are two results' do
       example_frame = Frame.new
-      example_frame.ball_1_pins = 0
-      example_frame.ball_2_pins = 4
-      expect(example_frame.total_pins).to eq(4)
+      example_frame.add_pins_ball1(7)
+      example_frame.add_pins_ball2(2)
+      expect(example_frame.total_pins).to eq(9)
     end
   end
 end
